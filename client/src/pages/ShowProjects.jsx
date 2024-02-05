36c3b7737840a646d76fdb1f2f672d9404e84fec
adrian1715 <adrian40001@gmail.com>
2024-02-05 17:48:58 -0300

    ShowPages component fix

--- client/src/pages/ShowProjects.jsx
+++ client/src/pages/ShowProjects.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import { links } from "../App";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, Outlet } from "react-router-dom";
 import { formatLinkPath } from "../utils/formatLinkPath";
 import { toLinkFormatter } from "../utils/toLinkFormatter";
+import { isCurrentPage } from "../utils/isCurrentPage";
 
 export default function ShowProjects() {
+  // const pathname = useLocation().pathname.endsWith("/")
+  //   ? useLocation().pathname.substring(0, useLocation().pathname.length - 1)
+  //   : useLocation().pathname;
   const { pathname } = useLocation();
 
-  const projects = links
+  const matchingRoutes = links
     .filter((link) => link.path === "/projects")[0]
-    .children.filter(
-      (link) => `/projects/${link.path}` === pathname
-    )[0].children;
-  console.log(projects);
+    .children.map((project) => project.path);
+  const lastDirectory = pathname.split("/")[pathname.split("/").length - 1];
+
+  const isRouteAllowed = matchingRoutes.some((route) =>
+    route.includes(lastDirectory)
+  );
+
+  const directories = pathname.split("/");
+  // console.log(directories);
+
+  const projects = isRouteAllowed
+    ? links
+        .filter((link) => link.path === "/projects")[0]
+        .children.filter((link) => lastDirectory === link.path)[0].children
+    : links.filter((link) => link.path === "/projects")[0].children;
+
+  const projectUrls = isRouteAllowed
+    ? links.filter((link) => link.path === "/projects")[0].children
+    : projects
+        .filter((project) => project.path === directories[2])[0]
+        .children.map((project) => project.path);
+
+  console.log("Projects Urls", projectUrls);
+  if (isCurrentPage(pathname, projectUrls)) return <Outlet />;
 
   return (
     <>
